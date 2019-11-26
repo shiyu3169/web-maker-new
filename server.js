cee81d6e7876be23a015d5ec5fb096da311ba930
Shiyu Wang <shiyu3169@gmail.com>
2019-11-26 09:39:30 -0500

    send server code

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+app.use(express.json());
+
+// Serve static assets in production
+if (process.env.NODE_ENV === "production") {
+  // Set static folder
+  app.use(express.static("client/build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
+
+const PORT = process.env.PORT || 3100;
+
+app.listen(PORT, () => {
+  console.log(`Server started on port ${PORT}`);
+});
