b97e65e02c4e5b1857ad7c076d04bab855df521d
Shiyu Wang <shiyu3169@gmail.com>
2019-12-19 09:25:34 -0500

    link to mlab

--- config/db.js
+++ config/db.js
@@ -4,11 +4,22 @@ const config = require("config");
 const db = config.get("mongo");
 
 async function connectDB() {
-  await mongoose.connect(db, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useFindAndModify: false
-  });
+  if (process.env.NODE_ENV === "production") {
+    const username = process.env.MLAB_USERNAME;
+    const password = process.env.MLAB_PASSWORD;
+    let connection = `mongodb://${username}:${password}${db}`;
+    await mongoose.connect(connection, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+  } else {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false
+    });
+  }
 }
 
 module.exports = connectDB;
